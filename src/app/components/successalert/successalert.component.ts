e44179c64114c0548b81047a7a0641477f685b2e
rpolacheck <rpolacheck@gmail.com>
2019-08-25 14:43:45 -0500

    Add 3 new components, add bootstrap

---
+++ src/app/components/successalert/successalert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successalert',
+  templateUrl: './successalert.component.html',
+  styleUrls: ['./successalert.component.css']
+})
+export class SuccessalertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    alert('success');
+  }
+
+}
